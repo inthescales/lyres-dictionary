ebbfd660b71159002865f7ad436f2a4bc533912f
Robin Hill <rehill012@gmail.com>
2018-11-27 23:25:22 -0500

    Morphs update themselves for their exceptions when joined

--- lyre.py
+++ lyre.py
@@ -108,9 +108,10 @@ class Morph:
     def __init__(self, key):
         global morphary
         
-        self.morph = morphary.morph_for_key[key]
+        self.base = morphary.morph_for_key[key]
         self.prev = None
         self.next = None
+        self.refreshMorph()
         
     def __eq__(self, other):
         if other == None:
@@ -121,6 +122,37 @@ class Morph:
     def join(self, next_morph):
         self.next = next_morph
         next_morph.prev = self
+        self.refreshMorph()
+        next_morph.refreshMorph()
+        
+    def refreshMorph(self):
+        
+        def apply(case):
+            for key, value in case.items():
+                if key != "case":
+                    self.morph[key] = value
+        
+        self.morph = self.base.copy()
+        self.morph["exception"] = ""
+
+        if "exception" in self.base:
+            for exception in self.base["exception"]:
+                # Assume there's a match, and negate that if it doesn't meet a requirement
+                # Match the first case that we fill
+                match = True
+                case = exception["case"]
+
+                if "precedes" in case and self.next:
+                    for element in case["precedes"]:
+                        if self.next.morph["key"] == element:
+                            apply(exception)
+                            return
+
+                if "follows" in case and self.prev:
+                    for element in case["follows"]:
+                        if self.prev.morph["key"] == element:
+                            apply(exception)
+                            return
     
     def get_form(self):
         
@@ -135,127 +167,77 @@ class Morph:
             last_morph = self.prev.morph
         else:
             last_morph = None
-        
-        # Check for exceptional forms
-        excepted = False
-        if "exception" in self.morph:
-            for exception in self.morph["exception"]:
+    
+        # Get the proper form of the morph
+        if self.next != None:
 
-                # Assume there's a match, and negate that if it doesn't meet a requirement
-                # Match the first case that we fill
-                match = True
-                case = exception["case"]
+            # Follow special assimilation rules if there are any
+            if "assimilation" in self.morph:
 
-                # TODO - this doesn't make sense, since each case has its own base/link
-                if "precedes" in case:
-                    
-                    if not next_morph:
-                        match = False
+                next_letter = list(next_morph["key"])[0]
+
+                matched_case = None
+                star_case = None
+
+                for case, sounds in self.morph["assimilation"].items():
+
+                    if "*" in sounds:
+                        star_case = case
+
+                    if next_letter in sounds:
+                        matched_case = case
+                        break
+
+                if matched_case:
+                    case = matched_case
+                elif star_case:
+                    case = star_case
+
+                if case == "link":
+                    form = self.morph["link"]
+                elif case == "link-assim":
+                    form = self.morph["link-assim"]
+                elif case == "cut":
+                    form = self.morph["link"] + "-"
+                elif case == "double":
+                    form = self.morph["link-assim"] + next_letter
+                elif case == "nasal":
+                    if next_letter == 'm' or next_letter == 'p' or next_letter == 'b':
+                        form = self.morph["link-assim"] + 'm'
                     else:
-                        precede_match = False
-                        for element in case["precedes"]:
-                            if next_morph["key"] == element:
-                                precede_match = True
-                                break
-
-                        if not precede_match:
-                            match = False
-                            
-                if "follows" in case:
-                    
-                    if not last_morph:
-                        match = False
-                    else:
-                        follow_match = False
-                        for element in case["follows"]:
-                            if last_morph["key"] == element:
-                                follow_match = True
-                                break
-
-                        if not follow_match:
-                            match = False
-
-                if match:
-                    if next_morph == None:
-                        form = exception["final"]
-                    else:
-                        form = exception["link"]
-                    excepted = True
-                    break
-            
-                
-        if not excepted:
-
-            # Get the proper form of the morph
-            if self.next != None:
-
-                # Follow special assimilation rules if there are any
-                if "assimilation" in self.morph:
-
-                    next_letter = list(next_morph["key"])[0]
-
-                    matched_case = None
-                    star_case = None
-
-                    for case, sounds in self.morph["assimilation"].items():
-
-                        if "*" in sounds:
-                            star_case = case
-
-                        if next_letter in sounds:
-                            matched_case = case
-                            break
-
-                    if matched_case:
-                        case = matched_case
-                    elif star_case:
-                        case = star_case
-
-                    if case == "link":
-                        form = self.morph["link"]
-                    elif case == "link-assim":
-                        form = self.morph["link-assim"]
-                    elif case == "cut":
-                        form = self.morph["link"] + "-"
-                    elif case == "double":
-                        form = self.morph["link-assim"] + next_letter
-                    elif case == "nasal":
-                        if next_letter == 'm' or next_letter == 'p' or next_letter == 'b':
-                            form = self.morph["link-assim"] + 'm'
-                        else:
-                            form = self.morph["link-assim"] + 'n'
-                    else:
-                        form = case
-
-
-                # Default rules
+                        form = self.morph["link-assim"] + 'n'
                 else:
+                    form = case
 
-                    # Usually we'll use link form
-                    if "link" in self.morph:
-                        form = self.morph["link"]
 
-                    # Verbs or verbal derivations need to take participle form into account
-                    elif self.morph["type"] == "verb" or (self.morph["type"] == "derive" and self.morph["to"] == "verb"):
-                        if next_morph and "participle-type" in next_morph:
-                            if next_morph["participle-type"] == "present":
-                                form = self.morph["link-present"]
-                            elif next_morph["participle-type"] == "perfect":
-                                form = self.morph["link-perfect"]
-                        elif "link-verb" in morph:
-                            form = self.morph["link-verb"]
-                        else:
-                            form = self.morph["link-perfect"]
-
-                    # Use final form if nothing overrides
-                    else:
-                        form = self.morph["final"]
-        
-            # The final morph form
+            # Default rules
             else:
-                if not "final" in self.morph:
-                    print(self.morph)
-                form = self.morph["final"]
+
+                # Usually we'll use link form
+                if "link" in self.morph:
+                    form = self.morph["link"]
+
+                # Verbs or verbal derivations need to take participle form into account
+                elif self.morph["type"] == "verb" or (self.morph["type"] == "derive" and self.morph["to"] == "verb"):
+                    if next_morph and "participle-type" in next_morph:
+                        if next_morph["participle-type"] == "present":
+                            form = self.morph["link-present"]
+                        elif next_morph["participle-type"] == "perfect":
+                            form = self.morph["link-perfect"]
+                    elif "link-verb" in morph:
+                        form = self.morph["link-verb"]
+                    else:
+                        form = self.morph["link-perfect"]
+
+                # Use final form if nothing overrides
+                else:
+                    form = self.morph["final"]
+
+        # The final morph form
+        else:
+            if not "final" in self.morph:
+                print(self.morph)
+            form = self.morph["final"]
         
         return form
     
@@ -353,7 +335,7 @@ class Word:
 
             # Special chance to add prefixes before verbs.
             # Necessary to inflate their frequency given their small number.
-            if self.size() == 1 and current_type == "verb" and not last_morph.has_tag("no-prep") and not first_morph.get_type() in ["prep", "prefix"] and random.randint(0, 3) == 0:
+            if self.size() == 1 and current_type == "verb" and not last_morph.has_tag("no-prep") and not first_morph.get_type() in ["prep", "prefix"] and (random.randint(0, 3) == 0 or last_morph.has_tag("always-prep")):
                 new_morph = Morph( random.choice(morphary.type_morphs["prep"]) )
                 new_morph.join(first_morph)
                 self.morphs = [new_morph] + self.morphs
@@ -551,6 +533,8 @@ def is_vowel(letter):
 def is_consonant(letter):
     return not is_vowel(letter)
 
+# Word post-processing
+
 def rephrase(string, mode):
     
     words = string.split(" ")    
@@ -620,6 +604,9 @@ def run(count):
     
 def test(keys):
 
+    if needs_setup():
+        setup()
+        
     word = Word()
     word.set_keys(keys)
     
@@ -636,4 +623,7 @@ def generate_entry():
     return word.entry()
 
 run(10)
-#test(["mis", "ad", "dormire"])
+#test(["manus", "ify", "or"])
+
+# errors:
+# - asspect
