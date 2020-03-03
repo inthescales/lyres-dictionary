00ce2fb075e0838df13ec416877a588f5376f66a
Robin Hill <rehill012@gmail.com>
2020-03-02 19:52:57 -0500

    Add suffix filter method to morphothec

--- src/morphothec.py
+++ src/morphothec.py
@@ -20,7 +20,7 @@ class Morphothec:
                 raw_morphs = json.load(morph_data)
                 for morph in raw_morphs:
 
-                    if not validate_morph(morph):
+                    if not self.validate_morph(morph):
                         if "key" in morph:
                             print("ERROR - invalid morph for key " + morph["key"])
                         else:
@@ -59,7 +59,11 @@ class Morphothec:
                     print("Exiting with " + str(errors) + " validation errors")
                     exit(0)
                     
-    def filter_morphs(self, morph_type, morph_filter):
+    def filter_type(self, morph_type, morph_filter=None):
+
+        if morph_filter is None:
+            return self.type_morphs[morph_type]
+        
         selected = []
         for morph in self.type_morphs[morph_type]:
             if expressions.evaluate_expression(morph_filter, self.morph_for_key[morph]):
@@ -67,53 +71,65 @@ class Morphothec:
         
         return selected
 
-def validate_morph(morph):
-    
-    if not "key" in morph:
-        print(" - key is missing")
-        return False
-    
-    if not "type" in morph:
-        print(" - type is missing")
-        return False
+    def filter_appends_to(self, base_type, morph_filter=None):
+        if morph_filter is None:
+            return self.morphs_from[base_type]
+        
+        selected = []
+        for morph in self.morphs_from[base_type]:
+            if expressions.evaluate_expression(morph_filter, self.morph_for_key[morph]):
+                selected.append(morph)
+        
+        return selected
 
-    if not "origin" in morph:
-        print(" - origin is missing")
-        return False
+    @classmethod
+    def validate_morph(cls, morph):
     
-    morph_type = morph["type"]
-    
-    if morph_type == "noun":
-        if not "link" in morph or not "declension" in morph:
-            print(" - noun must have 'link' and 'declension'")
-            return False
-        elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"])):
-            print(" - noun must have tag 'count' or 'mass'")
-            return False
-        elif morph["declension"] not in [0, 1, 2, 3, 4, 5]:
-            print(" - invalid declension '" + str(morph["declension"]) + "'")
+        if not "key" in morph:
+            print(" - key is missing")
             return False
     
-    elif morph_type == "adj":
-        if not "link" in morph or not "declension" in morph:
-            print(" - adjective must have 'link' and 'declension'")
+        if not "type" in morph:
+            print(" - type is missing")
             return False
-        elif morph["declension"] not in [0, 12, 3]:
-            print(" - invalid declension '" + str(morph["declension"]) + "'")
+
+        if not "origin" in morph:
+            print(" - origin is missing")
             return False
+    
+        morph_type = morph["type"]
+    
+        if morph_type == "noun":
+            if not "link" in morph or not "declension" in morph:
+                print(" - noun must have 'link' and 'declension'")
+                return False
+            elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"])):
+                print(" - noun must have tag 'count' or 'mass'")
+                return False
+            elif morph["declension"] not in [0, 1, 2, 3, 4, 5]:
+                print(" - invalid declension '" + str(morph["declension"]) + "'")
+                return False
+    
+        elif morph_type == "adj":
+            if not "link" in morph or not "declension" in morph:
+                print(" - adjective must have 'link' and 'declension'")
+                return False
+            elif morph["declension"] not in [0, 12, 3]:
+                print(" - invalid declension '" + str(morph["declension"]) + "'")
+                return False
         
-    elif morph_type == "verb":
-        if not ("link-present" in morph and "link-perfect" in morph and "final" in morph and "conjugation" in morph):
-            print(" - verbs require 'link-present', 'link-perfect', 'final', and 'conjugation'")
-            return False
+        elif morph_type == "verb":
+            if not ("link-present" in morph and "link-perfect" in morph and "final" in morph and "conjugation" in morph):
+                print(" - verbs require 'link-present', 'link-perfect', 'final', and 'conjugation'")
+                return False
         
-        if morph["conjugation"] not in [0, 1, 2, 3, 4]:
-            print(" - invalid conjugation '" + str(morph["conjugation"]) + "'")
-            return False
+            if morph["conjugation"] not in [0, 1, 2, 3, 4]:
+                print(" - invalid conjugation '" + str(morph["conjugation"]) + "'")
+                return False
     
-    elif morph_type == "derive":
-        if not ("from" in morph and "to" in morph):
-            print(" - derive morphs must have 'from' and 'to'")
-            return False
+        elif morph_type == "derive":
+            if not ("from" in morph and "to" in morph):
+                print(" - derive morphs must have 'from' and 'to'")
+                return False
     
-    return True
+        return True
