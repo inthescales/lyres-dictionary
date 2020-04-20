8bd7f1305ddb4643a06aed0e65863d999d975e2c
Robin Hill <rehill012@gmail.com>
2020-04-20 17:40:15 -0400

    Added more greek roots

--- src/transforms.py
+++ src/transforms.py
@@ -5,8 +5,8 @@ from src.morphothec import Morphothec
 
 def seed_word(word, morphothec):
     bag = [
-        ("latin", morphothec.root_count_for_language("latin")),
-        ("greek", morphothec.root_count_for_language("greek"))
+        ("latin", 0), #morphothec.root_count_for_language("latin")),
+        ("greek", 1) #morphothec.root_count_for_language("greek"))
     ]
     choice = helpers.choose_bag(bag)
     if choice == "latin":
