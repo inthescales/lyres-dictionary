ad51a7f3eb7fb0b0f63c99a9b74c250b110a4f79
Robin Hill <rehill012@gmail.com>
2020-12-26 20:09:20 -0500

    Moved morph validation into a separate file

---
+++ src/morph_validator.py
@@ -0,0 +1,68 @@
+import jsonschema
+
+def validate_morph(morph):
+
+    if not "key" in morph:
+        print(" - key is missing")
+        return False
+
+    if not "type" in morph:
+        print(" - type is missing")
+        return False
+
+    if not "origin" in morph:
+        print(" - origin is missing")
+        return False
+
+    morph_type = morph["type"]
+
+    # TODO - pull these requirements into per-language data
+    # TODO - make countability a property, not a tag
+    if morph_type == "noun":
+        if morph["origin"] == "latin":
+            if not "link" in morph or not "declension" in morph:
+                print(" - noun must have 'link' and 'declension'")
+                return False
+            elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"] or "singleton" in morph["tags"])):
+                print(" - noun must have tag 'count', 'mass', or 'singleton'")
+                return False
+            elif morph["declension"] not in [0, 1, 2, 3, 4, 5]:
+                print(" - invalid declension '" + str(morph["declension"]) + "'")
+                return False
+        elif morph["origin"] == "greek":
+            if not "link" in morph:
+                print(" - noun must have 'link'")
+                return False
+            elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"] or "singleton" in morph["tags"])):
+                print(" - noun must have tag 'count', 'mass', or 'singleton'")
+                return False
+
+    elif morph_type == "adj":
+        if morph["origin"] == "latin":
+            if not "link" in morph or not "declension" in morph:
+                print(" - adjective must have 'link' and 'declension'")
+                return False
+            elif morph["declension"] not in [0, 12, 3]:
+                print(" - invalid declension '" + str(morph["declension"]) + "'")
+                return False
+        elif morph["origin"] == "greek":
+            if not "link" in morph:
+                print(" - adjective must have 'link'")
+                return False
+
+    elif morph_type == "verb":
+        if morph["origin"] == "latin":
+            if not ("link-present" in morph and "link-perfect" in morph and "final" in morph and "conjugation" in morph):
+                print(" - verbs require 'link-present', 'link-perfect', 'final', and 'conjugation'")
+                return False
+
+            if morph["conjugation"] not in [0, 1, 2, 3, 4]:
+                print(" - invalid conjugation '" + str(morph["conjugation"]) + "'")
+                return False
+
+    elif morph_type == "derive":
+        if not ("from" in morph and "to" in morph):
+            print(" - derive morphs must have 'from' and 'to'")
+            return False
+
+    return True
--- src/morphothec.py
+++ src/morphothec.py
@@ -1,6 +1,7 @@
 import json
 
-import src.expressions as expressions   
+import src.expressions as expressions  
+import src.morph_validator as validator
     
 class Morphothec:
     
@@ -50,7 +51,7 @@ class Morphothec:
                 raw_morphs = json.load(morph_data)
                 for morph in raw_morphs:
 
-                    if not self.validate_morph(morph):
+                    if not validator.validate_morph(morph):
                         if "key" in morph:
                             print("ERROR - invalid morph for key " + morph["key"])
                         else:
@@ -69,7 +70,7 @@ class Morphothec:
                 if errors > 0:
                     print("Exiting with " + str(errors) + " validation errors")
                     exit(0)
-                    
+    
     def filter_type(self, morph_type, language="latin", morph_filter=None):
 
         if morph_filter is None:
@@ -99,71 +100,3 @@ class Morphothec:
             return 0
                   
         return len(self.languages[language].roots)
-    
-    @classmethod
-    def validate_morph(cls, morph):
-    
-        if not "key" in morph:
-            print(" - key is missing")
-            return False
-    
-        if not "type" in morph:
-            print(" - type is missing")
-            return False
-
-        if not "origin" in morph:
-            print(" - origin is missing")
-            return False
-    
-        morph_type = morph["type"]
-    
-        # TODO - pull these requirements into per-language data
-        # TODO - make countability a property, not a tag
-        if morph_type == "noun":
-            if morph["origin"] == "latin":
-                if not "link" in morph or not "declension" in morph:
-                    print(" - noun must have 'link' and 'declension'")
-                    return False
-                elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"] or "singleton" in morph["tags"])):
-                    print(" - noun must have tag 'count', 'mass', or 'singleton'")
-                    return False
-                elif morph["declension"] not in [0, 1, 2, 3, 4, 5]:
-                    print(" - invalid declension '" + str(morph["declension"]) + "'")
-                    return False
-            elif morph["origin"] == "greek":
-                if not "link" in morph:
-                    print(" - noun must have 'link'")
-                    return False
-                elif not ("tags" in morph and ("count" in morph["tags"] or "mass" in morph["tags"] or "singleton" in morph["tags"])):
-                    print(" - noun must have tag 'count', 'mass', or 'singleton'")
-                    return False
-    
-        elif morph_type == "adj":
-            if morph["origin"] == "latin":
-                if not "link" in morph or not "declension" in morph:
-                    print(" - adjective must have 'link' and 'declension'")
-                    return False
-                elif morph["declension"] not in [0, 12, 3]:
-                    print(" - invalid declension '" + str(morph["declension"]) + "'")
-                    return False
-            elif morph["origin"] == "greek":
-                if not "link" in morph:
-                    print(" - adjective must have 'link'")
-                    return False
-        
-        elif morph_type == "verb":
-            if morph["origin"] == "latin":
-                if not ("link-present" in morph and "link-perfect" in morph and "final" in morph and "conjugation" in morph):
-                    print(" - verbs require 'link-present', 'link-perfect', 'final', and 'conjugation'")
-                    return False
-
-                if morph["conjugation"] not in [0, 1, 2, 3, 4]:
-                    print(" - invalid conjugation '" + str(morph["conjugation"]) + "'")
-                    return False
-    
-        elif morph_type == "derive":
-            if not ("from" in morph and "to" in morph):
-                print(" - derive morphs must have 'from' and 'to'")
-                return False
-    
-        return True
