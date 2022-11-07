1f16ca90a1d849f011bd60c58f2cb3ea54f2501a
Robin Hill <rehill012@gmail.com>
2022-11-06 19:26:17 -0500

    Update installer to use venv

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 python3 -m venv env
 source env/bin/activate
-pip3 install -r ../requirements.txt
+pip3 install -r requirements.txt
 crontab -l > tmp
-echo "0 */2 * * * sh /var/www/bots/lyres-dictionary/scripts/run.sh" >> tmp
+echo "0 */2 * * * bash /var/www/bots/lyres-dictionary/scripts/run.sh" >> tmp
 crontab tmp
 rm tmp
