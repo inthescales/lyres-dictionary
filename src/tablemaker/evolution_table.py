083cae2e0c25f9a1d46fe9bf2db04f31c71f5135
Robin Hill <rehill012@gmail.com>
2023-12-19 12:00:14 -0500

    Add word lists for table maker

---
+++ src/tablemaker/evolution_table.py
@@ -0,0 +1,35 @@
+import src.diachronizer.diachronizer as diachronizer
+import src.diachronizer.engine.helpers as helpers
+import src.tablemaker.table as table
+
+from src.diachronizer.engine.helpers import Config
+
+def make_table_oe_ne(inputs):
+    config = Config(locked=True)
+
+    oe_phonemes = []
+    for input in inputs:
+        phonemes = diachronizer.oe_orth_to_oe_phone(input, config)
+        oe_phonemes.append(phonemes)
+
+    me_phonemes = []
+    for oe in oe_phonemes:
+        phonemes = diachronizer.oe_phone_to_me_phone(oe, config)
+        me_phonemes.append(phonemes)
+
+    modern_forms = []
+    for me in me_phonemes:
+        spelling = diachronizer.me_phone_to_ne_orth(me, config)
+        modern_forms.append(spelling)
+
+    oe_forms = [form.replace("|", "") for form in inputs]
+    oe_output = ["/" + "".join([x.value for x in word]) + "/" for word in oe_phonemes]
+    me_output = ["/" + "".join([x.value for x in word]) + "/" for word in me_phonemes]
+    output_table = table.make_table([
+        table.TableColumn("OE written", oe_forms),
+        table.TableColumn("OE phonemes", oe_output),
+        table.TableColumn("ME phonemes", me_output),
+        table.TableColumn("Modern form", modern_forms),
+    ])
+
+    return output_table
--- tablemaker.py
+++ tablemaker.py
@@ -1,15 +1,11 @@
 import getopt
 import sys
 
-import src.diachronizer.diachronizer as diachronizer
-import src.diachronizer.engine.helpers as helpers
-from src.diachronizer.engine.helpers import Config
 import src.tablemaker.table as table
+import src.tablemaker.evolution_table as evolution_table
 
-# Table Drawing =================================
-
-def make_table_oe(input):
-    inputs = [
+list_root = "assets/word_lists/"
+oe_inputs = [
         "stel|an",
         "bāt",
         "cȳð|an",
@@ -39,55 +35,53 @@ def make_table_oe(input):
         "nacod"
     ]
 
-    config = Config(locked=True)
-
-    oe_phonemes = []
-    for input in inputs:
-        phonemes = diachronizer.oe_orth_to_oe_phone(input, config)
-        oe_phonemes.append(phonemes)
-
-    me_phonemes = []
-    for oe in oe_phonemes:
-        phonemes = diachronizer.oe_phone_to_me_phone(oe, config)
-        me_phonemes.append(phonemes)
-
-    modern_forms = []
-    for me in me_phonemes:
-        spelling = diachronizer.me_phone_to_ne_orth(me, config)
-        modern_forms.append(spelling)
-
-    oe_output = ["/" + "".join([x.value for x in word]) + "/" for word in oe_phonemes]
-    me_output = ["/" + "".join([x.value for x in word]) + "/" for word in me_phonemes]
-    output_table = table.make_table([
-        table.TableColumn("OE written", inputs),
-        table.TableColumn("OE phonemes", oe_output),
-        table.TableColumn("ME phonemes", me_output),
-        table.TableColumn("Modern form", modern_forms),
-    ])
-
-    print(output_table)
-
 # Process command line input
 if __name__ == '__main__' and len(sys.argv) > 0:
     
-    input = None
-    
+    mode = None
+    list_path = None
+
     # Error cases
-    def error_no_input():
-        print("Error: Must provide input word")
+    def error_unrecognized_args():
+        print("Error: Unrecognized arguments")
+        sys.exit(1)
+    
+    def error_no_list():
+        print("Error: must specify a word list")
+        sys.exit(1)
+    
+    def error_no_mode():
+        print("Error: no mode specified")
         sys.exit(1)
     
     # Get args
     try:
-        opts, params = getopt.getopt(sys.argv[1:], "w:", ["word="])
+        opts, params = getopt.getopt(sys.argv[1:], "el:", ["evolution", "list="])
     except getopt.GetoptError:
-        error_no_input()
+        error_unrecognized_args()
         sys.exit(2)
 
     # Process args
     for opt, arg in opts:
-        if opt in ["-w", "--word"]:
-            input = arg
+        if opt in ["-e", "--evolution"]:
+            mode = "evolution"
+        elif opt in ["-l", "--list"]:
+            list_path = arg
 
-    make_table_oe(input)
-    # test()
+    if mode == "evolution":
+        if list_path == None:
+            error_no_list()
+        
+        path = list_root + list_path
+        if len(list_path) < 4 or list_path[-4:] != ".csv":
+            path += ".csv"
+
+        file = open(path)
+        raw = file.read()
+        words = raw.split(",\n")
+
+        table = evolution_table.make_table_oe_ne(words)
+
+        print(table)
+    else:
+        error_no_mode()
