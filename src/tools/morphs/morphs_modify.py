a0ed491deb86d811f532554797227633e6c1f978
Robin <rehill012@gmail.com>
2024-07-11 16:03:38 -0400

    Add morph modifier tool

---
+++ src/tools/morphs/morphs_modify.py
@@ -0,0 +1,39 @@
+import getopt
+import sys
+
+import morphs_files as file_tool
+import morphs_format as format
+
+def modify(morph):
+    # Write code to modify the morph here, returning the new version
+    return morph
+
+# Write groups to the specified filenames
+def write(morphs, filename):
+    asorted = format.sort(morphs)
+    formatted = format.format(asorted)
+    file_tool.write_formatted_to(formatted, filename)
+
+if __name__ == '__main__':
+    # Read args
+    try:
+        opts, params = getopt.getopt(sys.argv[1:], "", [])
+    except getopt.GetoptError:
+        print('ERROR: getopt error')
+        sys.exit(2)
+
+    files = params
+
+    if len(files) == 0:
+        print("ERROR: Must specify at least one morphs file")
+        sys.exit(0)
+
+    for file in files:
+        # Read in morphs
+        morphs = file_tool.get_morphs_from(file)
+
+        # Modify morphs
+        morphs = [modify(m) for m in morphs]
+
+        # Write back to file
+        write(morphs, file)
