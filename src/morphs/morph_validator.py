9b4f581e0e3a96cbba8919e9c6603b240eec07d0
Robin <rehill012@gmail.com>
2024-08-21 13:03:02 -0400

    Add a few morphs

--- src/morphs/morph_validator.py
+++ src/morphs/morph_validator.py
@@ -82,7 +82,7 @@ valid_tags = [
     "color",                    # Noun semantics - a color
     "shape",                    # Noun semantics - a shape
     "quality",                  # Noun semantics - an abstract quality
-    "state",                    # Noun semantics - a mental, physical, or emotional state
+    "mind-state",               # Noun semantics - a mental or emotional state
     "time",                     # Noun semantics - a period of time
     "tree",                     # Noun semantics - a kind of tree
     "number",                   # Noun semantics - a number (used as a noun)
