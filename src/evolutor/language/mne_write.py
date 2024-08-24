37586a7b06f1fa283fc6171b7813d4c0ccb7fe7a
Robin <rehill012@gmail.com>
2024-08-24 19:35:30 -0400

    Add morphs

--- src/evolutor/language/mne_write.py
+++ src/evolutor/language/mne_write.py
@@ -351,7 +351,7 @@ def from_me_phonemes(phonemes, config):
             elif (not prev or prev.is_vowel()) \
                 and next1 \
                 and next1.value != "n" \
-                and not next1.value in ["ɛ", "e", "i", "ɛː", "eː", "iː"] \
+                and not next1.value in ["ɛ", "e", "i", "ɛː", "eː", "iː", "ə"] \
                 and not insert_lengthening_e:
                 # 'c' when not followed by a /e/ or /e/
                 result += "c"
