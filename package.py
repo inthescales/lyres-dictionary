9fa09b828b75f995ca95ddf00df4b7f4164ab4bd
Robin <rehill012@gmail.com>
2024-12-03 10:37:57 -0500

    Add packaging script

---
+++ package.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+output_path = "./deploy"
+
+if os.path.exists(output_path) and os.path.isdir(output_path):
+    shutil.rmtree(output_path)
+
+os.mkdir(output_path)
+
+files = [
+	"lyre.py",
+	"requirements.txt",
+	"creds.json"
+]
+
+directories = [
+	"src",
+	"data"
+]
+
+print ("Copying files")
+
+for file in files:
+	shutil.copy(file, output_path + "/" + file)
+
+print ("Copying directories")
+
+for directory in directories:
+	shutil.copytree(directory, output_path + "/" + directory)
+
+print("Finished")
