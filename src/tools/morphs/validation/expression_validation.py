5f60facea56c40cd3c9c21c2b2f0a9ebb251a8b8
Robin <rehill012@gmail.com>
2025-06-25 11:35:02 -0400

    Rewrote type validation

--- src/tools/morphs/validation/expression_validation.py
+++ src/tools/morphs/validation/expression_validation.py
@@ -1,31 +1,8 @@
-from src.tools.morphs.schemas.expression_keys import expression_value_types
-from src.tools.morphs.validation.type_validation import type_match
-
-from src.tools.morphs.schemas.expression_keys import expression_keys as valid_expression_keys
+from src.tools.morphs.validation.type_validation import Meta, schemata
 
 # Validate the structure and value types of an expression (as in morph requirements and exceptions)
 def validate_expression(expression):
     errors = []
 
-    # Check that each expression contains only one key
-    if len(expression.items()) > 1:
-        errors.append("expressions may only have one key, but found key " + str(len(expression.items())) + ": " + str(expression))
-
-    for key, value in expression.items():
-        # Check that only valid keys appear
-        if key not in valid_expression_keys:
-            errors.append("invalid expression key \"" + key + "\" in expression: " + str(expression))
-
-        # Call type validation
-        type_errors = type_match(value, expression_value_types[key], key, expression)
-        errors += type_errors
-
-        # Recurse on sub-expressions
-        if type(value) == dict:
-            errors += validate_expression(value)
-        elif type(value) == list:
-            for subvalue in value:
-                if type(subvalue) == dict:
-                    errors += validate_expression(subvalue)
-
-    return errors
+    meta = Meta("root", expression, schemata)
+    return schemata["expression"].get_errors(expression, meta)
--- src/tools/morphs/validation/type_validation.py
+++ src/tools/morphs/validation/type_validation.py
@@ -1,126 +1,214 @@
 from src.tools.morphs.schemas.properties import properties as valid_properties
 from src.tools.morphs.schemas.tags import tags as valid_tags
-from src.tools.morphs.schemas.types import morph_types as valid_morph_types
+from src.tools.morphs.schemas.types import morph_types as valid_types
 
 # Placeholder types =================================
 
 # These represent different types of values that appear in the data files, which
 # are used for both type and value validation.
 
+class Type:
+    def is_valid(self, value, meta):
+        return self.get_errors(value, meta)
+
+class Meta:
+    def __init__(self, key, expression, schemata):
+        self.key = key
+        self.expression = expression
+        self.schemata = schemata
+
+class Primitive(Type):
+    name = "generic primitive"
+
+    def __init__(self, type, valueset=None):
+        self.type = type
+        self.valueset = valueset
+
+    def get_errors(self, value, meta):
+        if type(value) != self.type:
+            return ["invalid value type for key '" + meta.key + "' in dict " + str(meta.expression) + ". Expected type " + self.name]
+        elif self.valueset != None and value not in self.valueset.values:
+            return ["invalid " + self.valueset.name + " '" + value + "' found in dict " + str(meta.expression) + "."]
+
+        return []
+
+class Bool(Primitive):
+    name = "boolean"
+
+    def __init__(self, valueset=None):
+        Primitive.__init__(self, bool, valueset)
+
+class Integer(Primitive):
+    name = "integer"
+    def __init__(self, valueset=None):
+        Primitive.__init__(self, int, valueset)
+
+class String(Primitive):
+    name = "string"
+
+    def __init__(self, valueset=None):
+        Primitive.__init__(self, str, valueset)
+
+class Collection(Type):
+    name = "generic collection"
+
+    def __init__(self, subtype):
+        self.subtype = subtype
+
+class Array(Collection):
+    name = "list"
+
+    def get_errors(self, value, meta):
+        if type(value) != list:
+            return ["invalid value type for key '" + meta.key + "' in dict '" + str(meta.expression) + "'. Expected type " + type_name(self)]
+
+        errors = []
+        for item in value:
+            child_errors = self.subtype.get_errors(item, meta)
+            if len(child_errors) > 0 and issubclass(type(self.subtype), Primitive):
+                errors += ["invalid list member '" + str(item) + "' for key '" + meta.key + "' in dict '" + str(meta.expression) + "'. List entries should be " + type_name(self.subtype, True)]
+            else:
+                errors += child_errors
+
+        return errors
+
 # May be a single value, or a list of values
-class one_or_more:
-    def __init__(self):
-        return
+class one_or_more(Collection):
+    name = "one-or-list"
 
-# A string which is a valid tag
-class tag:
-    def __init__(self):
-        return
+    def get_errors(self, value, meta):
+        if type(value) == list:
+            return Array(self.subtype).get_errors(value, meta)
+        
+        errors = self.subtype.get_errors(value, meta)
+        if len(errors) > 0 and issubclass(type(self.subtype), Primitive):
+            errors = ["invalid value '" + str(value) + "' for key '" + meta.key + "' in dict '" + str(meta.expression) + "'. Expected type " + type_name(self)]
 
-# A string which is a valid morph type (e.g. noun, verb)
-class mtype:
-    def __init__(self):
-        return
+        return errors
 
-# A string which is a valid morph property
-class mprop:
-    def __init__(self):
-        return
+class Dict(Type):
+    name = "dictionary"
 
-# Base types that should be used when comparing the actual type of the data instance
-base_types = {
-    mprop: str,
-    mtype: str,
-    tag: str
+    def __init__(self, reference, restrict=True):
+        self.reference = reference
+        self.restrict = restrict
+
+    def key_match(self, value, meta):
+        return type(value) == dict and all([type(exp) == Opt or key in value.keys() for key, exp in self.reference.items()])
+
+    def get_errors(self, value, meta):
+        errors = []
+
+        # Check that value is a dict
+        if type(value) != dict:
+            return ["invalid value type for key '" + meta.key + "' in dict '" + str(meta.expression) + "'. Expected dictionary with reference key '" + self.reference + "'"]
+
+        # Check that all required keys are present
+        missing_keys = [key for key, exp in self.reference.items() if type(exp) != Opt and key not in value.keys()]
+        if len(missing_keys) > 0:
+            errors += ["missing keys '" + str(missing_keys) + "' for dictionary in expression '" + str(meta.expression) + "'"]
+
+        # If restricted, check that no keys outside the spec are present
+        if self.restrict:
+            extra_keys = [key for key in value.keys() if key not in self.reference.keys()]
+            if len(extra_keys) > 0:
+                errors += ["unrecognized keys " + str(extra_keys) + " found for dictionary '" + str(self.reference) + "' in expression '" + str(meta.expression) + "'"]
+
+        # Check that values have the expected types
+        for key, exp in self.reference.items():
+            if key in value:
+                new_meta = Meta(key, value, meta.schemata)
+                errors += exp.get_errors(value[key], new_meta)
+
+        return errors
+
+class Schema(Type):
+    def __init__(self, schema_key):
+        self.schema_key = schema_key
+        self.name = "schema with key '" + self.schema_key + "'"
+
+    def get_errors(self, value, meta):
+        if self.schema_key not in meta.schemata:
+            return ["Unknown schema key '" + self.schema_key + "' in dictionary specification"]
+
+        return meta.schemata[self.schema_key].get_errors(value, meta)
+
+class Opt(Type):
+    name = "optional"
+
+    def __init__(self, base_type):
+        self.base_type = base_type
+
+    def get_errors(self, value, meta):
+        if value == None:
+            return []
+        else:
+            return self.base_type.get_errors(value, meta)
+
+class Any(Type):
+    name = "any"
+
+    def __init__(self, items):
+        self.items = items
+
+    def get_errors(self, value, meta):
+        # If we have a key match, assume that that was the intended expansion
+        for option in self.items:
+            if type(option) == Dict and option.key_match(value, meta):
+                return option.get_errors(value, meta)
+
+        return ["invalid value type for key '" + meta.key + "' in expression '" + str(meta.expression) + "'."]
+
+class ValueSet:
+    def __init__(self, name, values):
+        self.name = name
+        self.values = values
+
+# Schemata ===========================================
+
+valueset_prop = ValueSet("property", valid_properties)
+valueset_tag  = ValueSet("tag", valid_tags)
+valueset_type = ValueSet("type", valid_types)
+
+                                      # ARGUMENT TYPE             EVALUATION RESULT
+schemata = {
+    "expression": Any([
+        Dict({"or":                   Array(Schema("expression"))}),      # true if any expression in list evaluates true
+        Dict({"and":                  Array(Schema("expression"))}),      # true if all expressions in list evaluate true
+        Dict({"not":                  Schema("expression")}),             # true if the expression evaluates false
+        Dict({"has-key":              one_or_more(String())}),          # true if the morph's key matches the given string, or is in the list
+        Dict({"has-type":             one_or_more(String(valueset_type))}), # true if the morph's type is equal to the string, or is in the list
+        Dict({"has-property":         String(valueset_prop)}),              # true if the morph contains a value for the given property
+        Dict({"has-tag":              String(valueset_tag)}),               # true if the morph contains the given tag
+        Dict({"has-all-tags":         Array(String(valueset_tag))}),        # true if the morph contains all of the given tags
+        Dict({"has-any-tags":         Array(String(valueset_tag))}),        # true if the morph contains any of the given tags
+        Dict({"has-prefix":           one_or_more(String())}),          # true if the morph's form begins with the given string, or any in a given list
+        Dict({"has-suffix":           one_or_more(String())}),          # true if the morph's form ends with the given string, or any in a given list
+        Dict({"has-suffix-template":  one_or_more(String())}),          # true if the morph's form's ending matches the given string template, or any in a given list
+                                                                        #   - C: matches any consonant
+                                                                        #   - V: matches any vowel
+                                                                        #   - other characters match as literals
+        Dict({"has-conjugation":      one_or_more(Integer())}),          # true if the morph has a conjugation equal to the given integer
+        Dict({"has-declension":       one_or_more(Integer())}),          # true if the morph has a declension equal to the given integer
+        Dict({"syllable-count":       Integer()}),                      # true if the morph
+        Dict({"is-root":              Bool()}),                         # true if the morph's type is a root type
+        Dict({"is-final":             Bool()}),                         # true if no morphs follow this morph
+        Dict({"final-or-semifinal-l": Bool()})                          # true if there is an l in the last two syllables
+    ])
 }
 
-# Returns the base type of the given placeholder type
-def base_type(t):
-    if t in base_types:
-        return base_types[t]
-    else:
-        return t
-
-# Container representing a value type, which may be a collection containing values of another type.
-class ValueType:
-    def __init__(self, maintype, subtype=None):
-        self.type = maintype
-        self.subtype = subtype
-        self.base_type = base_type(maintype)
-        self.base_subtype = base_type(subtype)
-
-        if subtype != None:
-            self.sub_as_main = ValueType(subtype)
-
-# Validation functions =============================
+# Validation helpers =============================
 
 # Get a name for the given type suitable for printing in error messages
 def type_name(expected, plural=False):
-    names = {
-        str: "string",
-        int: "integer",
-        mprop: "property",
-        mtype: "morph type",
-        tag: "tag",
-        list: "list",
-        one_or_more: "one-or-list",
-        dict: "expression dict" 
-    }
-
-    base_string = str(expected.type)
-    if expected.type in names:
-        base_string = names[expected.type]
+    name = expected.name
+    if issubclass(type(expected), Primitive) and expected.valueset != None:
+        name = "(" + expected.name +") " + expected.valueset.name
 
     if plural:
-        base_string += "s"
+        name += "s"
 
-    if expected.subtype != None:
-        base_string += " of " + type_name(expected.sub_as_main, plural=True)
+    if issubclass(type(expected), Collection):
+        name += " of " + type_name(expected.subtype, True)
 
-    return base_string
-
-# For types with restricted values, such as tags, confirms that the value is valid for that type.
-def check_values(value, expected, key=None, expression=None):
-    errors = []
-
-    def check_as(t):
-        return expected.type == t or (expected.type == one_or_more and expected.subtype == t and type(value) == expected.base_subtype)
-
-    if check_as(tag) and value not in valid_tags:
-        errors.append("invalid tag '" + str(value) + "' in expression: " + str(expression))
-
-    if check_as(mtype) and value not in valid_morph_types:
-        errors.append("invalid morph type '" + str(value) + "' in expression: " + str(expression))
-
-    if check_as(mprop) and value not in valid_properties:
-        errors.append("invalid morph property '" + str(value) + "' in expression: " + str(expression))
-
-    return errors
-
-# Checks whether the value has a type that matches the expected type, returning any errors found.
-# This takes subtypes into account (e.g. expected type 'tag' matches any string that's in the valid tags list)
-def type_match(value, expected, key=None, expression=None, is_member=False):
-    errors = []
-    
-    # Check base type match
-    if expected.type == one_or_more and expected.subtype != None:
-        type_matches = type(value) in [list, expected.base_subtype]
-    else:
-        type_matches = type(value) == expected.base_type
-
-    if not type_matches:
-        if not is_member:
-            errors.append("invalid value type for expression key \"" + key + "\" in expression: " + str(expression) +". Value should be " + type_name(expected) + "")
-        else:
-            errors.append("invalid value type for key \"" + key + "\" in expression: " + str(expression) +". List entries should be " + type_name(expected, plural=True))
-
-    # Check matches for collection members
-    if expected.subtype != None and type(value) == list:
-        for member in value:
-            errors += type_match(member, expected.sub_as_main, key, expression, True)
-
-    # Check values for types with value restrictions
-    if len(errors) == 0:
-        errors += check_values(value, expected, key, expression)
-
-    return errors
+    return name
