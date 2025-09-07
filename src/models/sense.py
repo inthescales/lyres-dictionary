a47ab41e5bff1fd38db8a0cddd8662c68e460a18
Robin <rehill012@gmail.com>
2025-09-07 18:59:05 -0400

    Add sense model (should have added it before)

---
+++ src/models/sense.py
@@ -0,0 +1,8 @@
+class Sense:
+    def __init__(self, sdict, language):
+        self.dict = sdict
+
+        if "period" in sdict:
+            self.period = sdict["period"]
+        else:
+            self.period = default_period_for(language)
