c624a3bfe7b02250dedbaa3b57246ce404a15f08
Robin <rehill012@gmail.com>
2025-09-10 14:22:47 -0400

    Programmatically convert OE forms with a single raw form and a canon form

--- src/tools/morphs/hardcoded/modify.py
+++ src/tools/morphs/hardcoded/modify.py
@@ -1,12 +1,13 @@
 def modify(morph):
     # Write code to modify the morph here, returning the new version
     if "form-raw" in morph \
-        and "form-canon" not in morph \
+        and "form-canon" in morph \
         and "form-raw-alt" not in morph \
         and "form-participle-raw" not in morph \
         and "form-participle-canon" not in morph \
         and type(morph["form-raw"]) == str:
-        morph["form-oe"] = morph["form-raw"]
+        morph["form-oe"] = { "form": morph["form-raw"], "canon": morph["form-canon"] }
         del morph["form-raw"]
+        del morph["form-canon"]
 
     return morph
