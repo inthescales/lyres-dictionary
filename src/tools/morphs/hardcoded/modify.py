5afcff4bf79d1b80e7ac8efcdb3846d0f409048b
Robin <rehill012@gmail.com>
2025-09-12 16:05:11 -0400

    Update OE morphs with participle overrides

--- src/tools/morphs/hardcoded/modify.py
+++ src/tools/morphs/hardcoded/modify.py
@@ -4,10 +4,13 @@ def modify(morph):
         and "form-canon" in morph \
         and "form-participle-canon" in morph \
         and "form-raw-alt" not in morph \
-        and "form-participle-raw" not in morph \
+        and "form-participle-raw" in morph \
         and type(morph["form-raw"]) == str:
         morph["form-oe"] = { 
-            "form": morph["form-raw"],
+            "form": {
+                "infinitive": morph["form-raw"],
+                "past-participle": morph["form-participle-raw"]
+            },
             "canon": {
                 "lemma": morph["form-canon"],
                 "past-participle": morph["form-participle-canon"]
@@ -15,6 +18,7 @@ def modify(morph):
         }
         del morph["form-raw"]
         del morph["form-canon"]
+        del morph["form-participle-raw"]
         del morph["form-participle-canon"]
 
     return morph
