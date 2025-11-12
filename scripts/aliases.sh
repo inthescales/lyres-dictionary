6f7ce950420a7aae29f768a77d4a1d5839a014e6
Robin <rehill012@gmail.com>
2025-11-12 12:34:27 -0500

    Add aliases.sh

---
+++ scripts/aliases.sh
@@ -0,0 +1,7 @@
+# Helper aliases for various scripts
+
+alias lyre="python3 lyre.py"
+alias lylint="python3 linter.py"
+alias morphs="python3 morphs.py"
+alias unit="python3 unit.py"
+alias table="python3 table.py"
