e6518956ec19f414e1e48f68814380cdc86f8c8e
Robin <rehill012@gmail.com>
2025-12-20 12:01:56 -0500

    Add Latin formsets file

---
+++ src/languages/latin/formsets.py
@@ -0,0 +1,8 @@
+from src.formset import FormSet
+
+class LatinVerbFormSet(FormSet):
+    def __init__(self, present: str, infinitive: str, perfect: str, supine: str):
+        self.present = present
+        self.infinitive = infinitive
+        self.perfect = perfect
+        self.supine = supine
